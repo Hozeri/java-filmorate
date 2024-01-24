73b566842f70bb66fab3b6c83faa1a296189e56c
Aleksey Karev <aleksey.karev@raiffeisen.ru>
2024-01-25 00:59:00 +0100

    refactor: remove redundant import

--- src/test/java/ru/yandex/practicum/filmorate/FilmDbStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmDbStorageTest.java
@@ -12,7 +12,6 @@ import ru.yandex.practicum.filmorate.repository.storageimpl.FilmDbStorage;
 
 import java.time.LocalDate;
 import java.util.LinkedHashSet;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
